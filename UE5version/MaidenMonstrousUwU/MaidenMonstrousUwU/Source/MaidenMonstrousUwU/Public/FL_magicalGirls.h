4423a76ff6a8ab91dc74c74ad1e7f8e848872c84
FélixChevrel <fchevrel@artfx.fr>
2022-11-25 13:33:39 +0100

    Begin CPP

---
+++ UE5version/MaidenMonstrousUwU/MaidenMonstrousUwU/Source/MaidenMonstrousUwU/Public/FL_magicalGirls.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FL_magicalGirls.generated.h"
+
+/**
+ * 
+ */
+
+namespace NSPlayerCharacters 
+{
+	UCLASS()
+		class MAIDENMONSTROUSUWU_API UFL_magicalGirls : public UBlueprintFunctionLibrary
+	{
+		GENERATED_BODY()
+
+	};
+}
+
